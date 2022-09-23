9e8bf1aa8aa3b541ecd7d92d73f15e915e9e1bf5
surya chauhan <chauhansurya3022gmail.com>
2022-09-23 19:59:05 +0530

    The code was inncorrect i corrected it

--- prime_Problem.js
+++ prime_Problem.js
@@ -5,8 +5,7 @@ function check_Prime(num)
 {
  for(let i=1;i<=num;i++){
    num>1 ?  num%i==0 ? factor++ : null :null; 
- }
-    ;
+ }factor>2 ? console.log("Not Prime!") :  factor==0 ? console.log("Not Prime!"): console.log("Prime!");
 }
 
 check_Prime(num);
